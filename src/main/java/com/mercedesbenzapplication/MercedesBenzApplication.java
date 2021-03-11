d46c729fd0b27e71438788ebc8aaf54e0303f00b
Himanshi Sharma <himanshi.sharma@hashedin.com>
2021-03-11 21:03:01 +0530

    Adding boilerplate code

---
+++ src/main/java/com/mercedesbenzapplication/MercedesBenzApplication.java
@@ -0,0 +1,13 @@
+package com.mercedesbenzapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercedesBenzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercedesBenzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercedesbenzapplication/MercedesBenzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercedesbenzapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercedesBenzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
